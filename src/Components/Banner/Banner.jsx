41be15d55d14ddaec36f3b35edb265a41d1343f4
AhmedSami019 <easami019192@gmail.com>
2025-05-01 16:52:27 +0600

    working on the banner section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,7 +6,7 @@ import bannerIcon2 from "../../assets/backup_table.png";
 
 const Banner = () => {
   return (
-    <div className="hero lg:flex gap-10">
+    <div className="flex gap-10 mx-10 justify-center relative">
       <div className=" w-1/2 p-10">
         <div className="mb-10">
           <h2 className="text-7xl mb-10">Your AI-Powered Sales Coach</h2>
@@ -25,7 +25,7 @@ const Banner = () => {
         <div className="flex gap-5 justify-around">
           <div className="flex items-center gap-4">
             <div className="p-4 bg-white shadow-xl rounded-xl">
-            <img src={bannerIcon1} alt="" />
+              <img src={bannerIcon1} alt="" />
             </div>
             <div>
               <h2 className="text-5xl">2000+</h2>
@@ -34,19 +34,31 @@ const Banner = () => {
           </div>
           <div className="flex items-center gap-4">
             <div className="p-4 bg-white shadow-xl rounded-xl">
-            <img src={bannerIcon2} alt="" />
+              <img src={bannerIcon2} alt="" />
             </div>
             <div>
               <h2 className="text-5xl">1700+</h2>
               <p className="text-xl">Provide Tailored</p>
             </div>
           </div>
-
-          
         </div>
       </div>
-      <div className=" w-1/2">
-        <img src={bannerImg} alt="" />
+      <div className=" w-1/2  ">
+        <div className="">
+          <img src={bannerImg} alt="" />
+        </div>
+        <div className="card lg:max-w-2/8 bg-base-100 card-lg shadow-2xl lg:absolute lg:-bottom-20 lg:right-3/10 ">
+          <div className="card-body">
+            <div className="flex justify-around ga-5  mb-8">
+            <h2 className="text-5xl font-medium">71+</h2>
+            <h2 className="text-5xl font-medium">1000+</h2>            
+            </div>
+            <p className="text-2xl font-medium mb-5">Growth is our priority.</p>
+            <p className="text-gray-500">
+            As a full-service business agency, we specialize in helping companies of all sizes optimize their operations
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
